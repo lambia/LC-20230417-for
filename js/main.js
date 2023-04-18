c309d549538cd1ffcc1e55daf825859acb6436e4
Luca Lambda <luca@lambia.it>
2023-04-18 11:56:51 +0200

    esercizio con bonus

--- js/main.js
+++ js/main.js
@@ -1,57 +1,49 @@
-// for (let i = 0; i < 10; i++) {
-  
-//     // i++ vuol dire =>    i = i +1;
-    
-//     let numeroDaVisualizzare = (i+1) * 2;
-
-//     console.log(`Contatore vale: ${i} - Noi stampiamo: ${numeroDaVisualizzare} - Contatore vale ancora: ${i}`,  );
-    
-// }
-
-// console.log("Tabellina del 2");
-
-// for (let i = 2; i <= 20; i = i+2) {
-//     console.log(i);
-// }
-
-// let datoEsatto = false;
-
-// while(datoEsatto == false) {
-//     let x = prompt("Inserisci un dato");
-//     if(x!=null && x.length>0) {
-//         datoEsatto = true;
-//     }
-// }
-
 const contentitore = document.getElementById("container");
-const ul = document.querySelector("ul.list");
 
-for (let contatore = 0; contatore < 100; contatore++) {
+// Contatore = numero
+for (let numero = 1; numero <= 100; numero++) {
+    
+    /* SOLUZIONE - ELSE-IF */
+    let messaggio = numero;
+    let classe = "";
+    // // if (numero % 3 == 0 && numero % 5 == 0) {
+    if (numero % 15 == 0) {
+        messaggio = "FizzBuzz";
+        classe = "blu";
+    } else if (numero % 3 == 0) {
+        messaggio = "Fizz";
+        classe = "rosso";
+    } else if (numero % 5 == 0) {
+        messaggio = "Buzz";
+        classe = "verde";
+    }
 
-    let ilNostroIndice = contatore + 1; //così 0 diventa 1, e 99 diventa 100
+    
+    /* POSSIBILE ALTERNATIVA MENO EFFICIENTE - TRIPLO IF */
+    // let messaggio = "";
+    // if(numero % 3 == 0) {
+        //     messaggio += "Fizz";
+    // }
+    // if (numero % 5 == 0) {
+        //     messaggio += "Buzz";
+    // }
+    // if(numero % 3 != 0 && numero % 5 != 0) {
+    //     messaggio = numero;
+    // }
 
-    let numero = ilNostroIndice * 3; //così 1 diventa 3, e 100 diventa 300
 
-    // contentitore.innerHTML = contentitore.innerHTML + `<div class="box">${numero}</div>`;
-    contentitore.innerHTML += `<div class="box">${numero}</div>`;
+    // PARTE FINALE, SCRITTURA SU DOM
 
-    const nuovoElementoInLista = document.createElement('li'); //Creo un list-item
-    nuovoElementoInLista.append(numero); //Append il numero al list-item
-    ul.append(nuovoElementoInLista); //Append il list-item alla unordered list
+    // contentitore.innerHTML += `<div class="box ${classe}">${messaggio}</div>`;
+
+    const nuovoBox = document.createElement('div'); //Creo un div
+    nuovoBox.className = `box ${classe}`;
+    // nuovoBox.style.backgroundColor = "red";
+    // nuovoBox.append(messaggio); //Inserisci il messaggio alla fine del contenuto del box
+    nuovoBox.innerText = messaggio;
+
+    contentitore.append(nuovoBox); //Append il list-item alla unordered list
+
+    console.log(messaggio);
     
 }
-
-console.log( contentitore.innerHTML );
-
-// for (let contatore = 100; contatore > 0; contatore--) {
-
-//     let numero = contatore * 3; //così 1 diventa 3, e 100 diventa 300
-
-//     // contentitore.innerHTML = contentitore.innerHTML + `<div class="box">${numero}</div>`;
-//     contentitore.innerHTML += `<div class="box">${numero}</div>`;
-
-//     const nuovoElementoInLista = document.createElement('li'); //Creo un list-item
-//     nuovoElementoInLista.append(numero); //Append il numero al list-item
-//     ul.append(nuovoElementoInLista); //Append il list-item alla unordered list
-    
-// }
